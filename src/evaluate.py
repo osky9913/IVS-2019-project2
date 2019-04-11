29504ec6c259b023cd7de2c4c4a0e2b09333b91d
osky9913 <m.osvald9913@gmail.com>
2019-04-11 07:14:06 +0200

    editing isNumber function

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,6 +6,12 @@ from pythonds.basic.stack import Stack #pip3 install --user  pythonds
 from mathlibrary import Mathlibrary
 
 
+"""
+@brief Validate Number
+@param s String Number ( float or int ) 
+@return boolean True or False
+@author Martin Osvald xosval03
+"""
 def isNumber(s):
 
 
@@ -16,7 +22,9 @@ def isNumber(s):
         if ( i.count('.') == 1 and len(i)== 1):
             return False
         
-
         if i not in "0123456789.":
             return False
+        if s[0] == '.' or s[-1]:
+            return False
+
     return True
