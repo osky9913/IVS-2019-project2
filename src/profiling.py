961cb75ca6722e10c303f6eeece3bbac1cbc8cb4
Marek Sarvas <xsarva00@stud.fit.vutbr.cz>
2019-04-19 12:55:27 +0200

    Adding working profiling with two options and graphs of both of them.

--- src/profiling.py
+++ src/profiling.py
@@ -14,54 +14,63 @@ import sys
 
 # create SUM in standard deviation formula as string
 # for evaluating single string
-def createSum(inputNumbers, subSum):
+def createSum(inputNumbers):
     total = "( "
     for x in inputNumbers.split():
-        total += "( " + x + " ^ 2 - " + subSum + " ) + "
+        total += "( " + x + " ^ 2" + " ) + "
     total = total[:-2]
-    total += ") "
+    total += ")"
     return total
 
 #  creating SUM of given numbers divided by count of numbers squared
 # for evaluating single string
 def createSubSum(inputNumbers):
-    totalSubSum = "( ( ( "
+    totalSubSum = "( ( ( ( "
     N = 0
     for x in inputNumbers.split():
         totalSubSum += x
         totalSubSum += ' + '
         N += 1
     totalSubSum = totalSubSum[:-3]
-    totalSubSum += " ) / " + str(N) + " ) ^ 2 * " + str(N) + " ) "
+    totalSubSum += " ) / " + str(N) + " ) ^ 2 ) * " + str(N) + " )"
     return totalSubSum
 
-# main
+#Main. oneString variable is used as switch to do profiling
+# either with one giant string or with some side calculations
 if __name__ == '__main__':
     inputNumbers=""
+    oneString = True
     N = 0 
+    #Read numbers from stdin
     for line in sys.stdin:
         inputNumbers += line
-        N += 1
+    #Replace eol with space
     inputNumbers = inputNumbers.replace('\n',' ')
+    #Count the numbers
+    for x in inputNumbers.split():
+        N += 1
+    
 
     #Profiling on one single string
-    #subSum = createSubSum(inputNumbers)
-    #totalSum = createSum(inputNumbers, subSum)
-    #evalString = "2 √ ( ( 1 / {} - 1 ) * {} )".format(N, totalSum)
-    #with PyCallGraph(output=GraphvizOutput()):
-    #    print(resolve(evalString))
+    if oneString:
+        subSum = createSubSum(inputNumbers)
+        nextSum = createSum(inputNumbers)
+        evalString = "2 √ ( ( 1 / ( {} - 1 ) ) * ( {} - {} ) )".format(N, nextSum, subSum)
+        with PyCallGraph(output=GraphvizOutput()):
+            print(resolve(evalString))
 
 
     #Profiling using side calculations
-    firstSum = 0
-    secondSum = 0
-    with PyCallGraph(output=GraphvizOutput()):
-        #Calculate SUM of numbers and SUM of numbers^2
-        for x in inputNumbers.split():
-            firstSum = resolve("{} + {}".format(firstSum, x))#sum of Xi
-            secondSum = resolve("{} + {} ^ 2".format(secondSum, x))
-        #N*(x')^2
-        firstSum = resolve("( {} / {} ) ^ 2 * {}".format(firstSum, N, N))
-        #final formula
-        print(resolve("2 √ ( 1 / ( {} - 1 ) * ( {} - {} ) )".format(N, secondSum, firstSum)))
+    else:
+        firstSum = 0
+        secondSum = 0
+        with PyCallGraph(output=GraphvizOutput()):
+            #Calculate SUM of numbers and SUM of numbers^2
+            for x in inputNumbers.split():
+                firstSum = resolve("{} + {}".format(firstSum, x))#sum of Xi
+                secondSum = resolve("{} + {} ^ 2".format(secondSum, x))
+            #N*(x')^2
+            firstSum = resolve("( {} / {} ) ^ 2 * {}".format(firstSum, N, N))
+            #final formula
+            print(resolve("2 √ ( 1 / ( {} - 1 ) * ( {} - {} ) )".format(N, secondSum, firstSum)))
 
